618e349b8320fd96f65575171f05ae0fb446c564
Phil Boyd <phil.boyd@live.com>
2020-06-28 14:22:25 -0400

    added sqs to stack

--- CDK/hello-world/lib/hello-world-stack.ts
+++ CDK/hello-world/lib/hello-world-stack.ts
@@ -1,9 +1,12 @@
 import * as cdk from '@aws-cdk/core';
+import * as iam from '@aws-cdk/aws-iam';
+import * as sqs from '@aws-cdk/aws-sqs';
 
 export class HelloWorldStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
 
-    // The code that defines your stack goes here
-  }
+        // The code that defines your stack goes here
+        const queue = new sqs.Queue(this, 'First-Queue');
+    }
 }
