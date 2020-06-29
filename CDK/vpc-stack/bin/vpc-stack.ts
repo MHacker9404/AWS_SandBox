e8fc4835949b26d0bc4e45e33061b29612975031
Phil Boyd <phil.boyd@live.com>
2020-06-29 18:08:31 -0400

    Lesson 17 complete

--- CDK/vpc-stack/bin/vpc-stack.ts
+++ CDK/vpc-stack/bin/vpc-stack.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
-import { VpcStackStack } from '../lib/vpc-stack-stack';
+import { VpcStack } from '../lib/vpc-stack';
 
 const app = new cdk.App();
-new VpcStackStack(app, 'VpcStackStack');
+new VpcStack(app, 'VpcStack');
