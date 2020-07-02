2c0cc649874069d37b278ba2c657031d8e6bdda9
Phil Boyd <phil.boyd@live.com>
2020-07-02 17:28:33 -0400

    lesson 19 complete

--- CDK/UltimateDevOps-CDK/bin/app.ts
+++ CDK/UltimateDevOps-CDK/bin/app.ts
@@ -3,9 +3,18 @@ import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
 import { VpcStack } from '../lib/vpc-stack';
 import { SecurityStack } from '../lib/security-stack';
+// import { MasterStack } from '../lib/master-stack';
+import { BastionStack } from '../lib/bastion-stack';
 
 const app = new cdk.App();
 let vpc_stack = new VpcStack(app, 'VpcStack');
 let sec_stack = new SecurityStack(app, 'SecurityStack', { vpc: vpc_stack.vpc });
+let bastion_stack = new BastionStack(app, 'BastionStack', { vpc: vpc_stack.vpc, securityGroup: sec_stack.bastion_sg });
+
+/*
+let master_stack = new MasterStack(app, 'MasterStack');
+let vpc_stack = new VpcStack(master_stack, 'VpcStack');
+let sec_stack = new SecurityStack(master_stack, 'SecurityStack', { vpc: vpc_stack.vpc });
+*/
 
 app.synth();
