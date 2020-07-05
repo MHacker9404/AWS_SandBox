3a1f5d7626b1936909c81e7d54516bf8e76854ee
Phil Boyd <phil.boyd@live.com>
2020-07-05 12:42:29 -0400

    lesson 20 complete

--- CDK/UltimateDevOps-CDK/bin/app.ts
+++ CDK/UltimateDevOps-CDK/bin/app.ts
@@ -5,11 +5,13 @@ import { VpcStack } from '../lib/vpc-stack';
 import { SecurityStack } from '../lib/security-stack';
 // import { MasterStack } from '../lib/master-stack';
 import { BastionStack } from '../lib/bastion-stack';
+import { KmsStack } from '../lib/kms-stack';
 
 const app = new cdk.App();
-let vpc_stack = new VpcStack(app, 'VpcStack');
-let sec_stack = new SecurityStack(app, 'SecurityStack', { vpc: vpc_stack.vpc });
-let bastion_stack = new BastionStack(app, 'BastionStack', { vpc: vpc_stack.vpc, securityGroup: sec_stack.bastion_sg });
+let vpc_stack = new VpcStack(app, 'vpc');
+let sec_stack = new SecurityStack(app, 'security', { vpc: vpc_stack.vpc });
+let bastion_stack = new BastionStack(app, 'bastion', { vpc: vpc_stack.vpc, securityGroup: sec_stack.bastion_sg });
+let kms_stack = new KmsStack(app, 'kms', {});
 
 /*
 let master_stack = new MasterStack(app, 'MasterStack');
